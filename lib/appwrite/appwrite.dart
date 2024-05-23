0387b3cad848474d9f002f35c55290ab4c9fc810
sajadwahid2002 <127975068+sajadwahid2002@users.noreply.github.com>
2024-05-23 13:40:21 +0300

    create account in appwrite

--- lib/appwrite/appwrite.dart
+++ lib/appwrite/appwrite.dart
@@ -1,11 +1,30 @@
 import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart';
+import 'package:flutter/material.dart';
 
 class Appwrite {
   Client client = Client();
-
+  late final Account account;
   Appwrite() {
     client
         .setEndpoint('https://cloud.appwrite.io/v1')
         .setProject('flutter-appwrite-tracker');
+    account = Account(client);
   }
+
+  Future<User?> createAccount(
+      String name, String email, String password) async {
+    try {
+      final user = await account.create(
+        userId: ID.unique(),
+        email: email,
+        password: password,
+      );
+      return user;
+    } on AppwriteException catch (error) {
+      debugPrint(error.message);
+      return null;
+    }
+  }
+  
 }
--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutterappwrite_tracker/appwrite/appwrite.dart';
 import 'package:flutterappwrite_tracker/features/signup/src/signup_screen.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 
 import '../pages/home_page.dart';
@@ -16,8 +20,15 @@ final routerProvider = Provider<GoRouter>((reference) {
       name: SignupScreen.name,
       path: '/signup',
       builder: (_, __) => SignupScreen(
-        onSignup: (name, email, password) {
+        onSignup: (name, email, password) async {
           debugPrint('$name - $email - $password');
+          final appwrite = GetIt.instance.get<Appwrite>();
+          final user = await appwrite.createAccount(
+            name,
+            email,
+            password,
+          );
+          debugPrint(jsonEncode(user ?? '{}'));
         },
       ),
     ),
